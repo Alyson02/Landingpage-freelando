f983faef0891b235c7a0da4995d59765df2b0cc2
Izabel Santos <izabeldsantos1@gmail.com>
2021-12-08 21:32:23 -0300

    página Quem Somos

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function telaRegistrar() {
+    window.location.href = "telaRegistrar.html";
+}
+
+function telaIndex() {
+    window.location.href = "telaLogin.php";
+}
+
+function telaCadastroContratante() {
+    window.location.href = "CadastroContratante.php";
+}
+
+function telaLogin() {
+    window.location.href = "telaLogin.php";
+}
+
+function telaCadastroProfissional() {
+    window.location.href = "CadastroProfissional.php";
+}
